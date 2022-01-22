da303377b7ff199937356533ae93e7c6fa54f45e
+Michael Kolb <+michael.w.kolb@gmail.com>
2022-01-21 19:47:25 -0500

    TC

---
+++ index.js
@@ -0,0 +1,79 @@
+function myEach(collection, callback) {
+    for (let accessor in collection) {
+        callback(collection[accessor]);
+    }
+    return collection;
+}
+
+function myMap(collection, callback) {
+    let modifiedArray = [];
+    for (let accessor in collection) {
+        modifiedArray.push(callback(collection[accessor]));
+    }
+    return modifiedArray;
+}
+
+function myReduce(collection, callback, acc) {
+    let copy = [...collection];
+    acc = acc || copy.shift();
+    for (let accessor in copy) {
+        acc = callback(acc, copy[accessor], copy);
+    }
+return acc;
+}
+
+function myFind(collection, callback) {
+    for (let accessor in collection) {
+        if (callback(collection[accessor])) {
+            return collection[accessor];
+        };
+    }
+}
+
+function myFilter(collection, callback) {
+    let selectedArray = [];
+    for (let accessor in collection) {
+        if (callback(collection[accessor])) {
+            selectedArray.push(collection[accessor]);
+        };
+    }
+return selectedArray;
+}
+
+function mySize(collection) {
+    return (Object.keys(collection).length);
+}
+
+function myFirst(collection, n) {
+    let tempArr = [];
+    let count = 1;
+
+    for (let accessor in collection) {
+        if (count <= n) {
+            tempArr.push(collection[accessor]);
+            count++;
+        }
+    }
+    return tempArr.length > 1 ? tempArr : tempArr[0];
+}
+
+function myLast(collection, n) {
+    let indexStart = collection.length - n;    
+    return n > 1 ? collection.slice(indexStart, collection.length) : collection[indexStart];
+}
+
+function myKeys(obj) {
+    let arr = [];
+    for (let keys in obj) {
+        arr.push(keys);
+    }
+    return arr;
+}
+
+function myValues(obj) {
+    let arr = [];
+    for (let values in obj) {
+        arr.push(obj[values]);
+    }
+    return arr;
+}
